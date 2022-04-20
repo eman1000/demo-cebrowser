ff56a74e14ff7f46b2760fb59cf6969059cc26c9
EM <emman@sam-media.com>
2022-04-20 17:38:58 +0800

    Image modal

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,21 @@
 $("#search-input-top").click(function(ev){
   ev.stopPropagation();
   $("#search-input-top").addClass("input-with-result-history");
-  $("#search-result-history-top").addClass("show");
+  $("#search-result-history-top").addClass("show_");
 });
 
 $("#search-input").click(function(ev){
   ev.stopPropagation();
   $("#search-input").addClass("input-with-result-history");
-  $("#search-result-history").addClass("show");
+  $("#search-result-history").addClass("show_");
 });
 
 
 $("body").click(function(){
-  $("#search-result-history-top").removeClass("show");
-  $("#search-result-history-top").addClass("hide");
-  $("#search-result-history").removeClass("show");
-  $("#search-result-history").addClass("hide");
+  $("#search-result-history-top").removeClass("show_");
+  $("#search-result-history-top").addClass("hide_");
+  $("#search-result-history").removeClass("show_");
+  $("#search-result-history").addClass("hide_");
   $("#search-input-top").removeClass("input-with-result-history");
   $("#search-input").removeClass("input-with-result-history");
 
@@ -24,3 +24,8 @@ $("body").click(function(){
 $('#apps-modal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+$(".scroll-arrow").click(()=>{
+  $("#suggestions").scrollLeft(0);
+})
+
